b1447865ca4c3cdd74a4f43972427a7aee16b102
andreasderuiter <mobileiq@hotmail.com>
2016-01-06 13:57:45 +0100

    Created app.ts for the main app module.

---
+++ bat-mvc/Scripts/App/app.ts
@@ -0,0 +1,17 @@
+// Install the angularjs.TypeScript.DefinitelyTyped NuGet package 
+
+module App {
+    "use strict";
+
+    // Create the module and define its dependencies.
+    angular.module("app", [
+        // Angular modules 
+        // "ngAnimate", // animations
+        // "ngRoute" // routing
+
+        // Custom modules 
+
+        // 3rd Party Modules
+    ]);
+}
+

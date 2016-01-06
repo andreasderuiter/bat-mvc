41574a0b0141929d487df99173339df8653a98e7
andreasderuiter <mobileiq@hotmail.com>
2016-01-06 14:43:39 +0100

    Initial Angular directive and completed step 3 (Web API)

---
+++ bat-mvc/Scripts/App/MyAppDirective.ts
@@ -0,0 +1,28 @@
+﻿module App {
+    "use strict";
+
+    interface IMyAppDirective extends ng.IDirective {
+    }
+
+    interface IMyAppDirectiveScope extends ng.IScope {
+    }
+
+    interface IMyAppDirectiveAttributes extends ng.IAttributes {
+    }
+
+    MyAppDirective.$inject = ["$window"];
+    function MyAppDirective($window: ng.IWindowService): IMyAppDirective {
+        return {
+            restrict: "EA",
+            link: link,
+            template: "<h1>Hello, world</h1>"
+
+        }
+
+        function link(scope: IMyAppDirectiveScope, element: ng.IAugmentedJQuery, attrs: IMyAppDirectiveAttributes) {
+
+        }
+    }
+
+    angular.module("app").directive("myApp", MyAppDirective);
+}
